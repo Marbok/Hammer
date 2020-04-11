34482859b3b7e774ef6b6c41e343aebbfefa5951
Dmitriev Andrey <dmitrievandryha94@gmail.com>
2020-04-11 10:56:26 +0300

    add package-info in generators

---
+++ src/main/java/beaninfo/inject_param/generators/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Generators (Gens). That's like mixin in Ruby.
+ * We have some methods and can include them in class.
+ * This package includes generators, which help to convert strings to definite data
+ */
+package beaninfo.inject_param.generators;
